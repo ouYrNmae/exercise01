ef9a8b2a90b88555168326a837a78be773300e78
ouYrNmae <148975441+ouYrNmae@users.noreply.github.com>
2025-01-16 13:41:18 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 //  Jocelin Martinez-Reyes
 //  1/16/25 Exercise01
 //  Q1: When computing the average high temperature, was the value stored as an integer value or a floating-point value?
-// ANSWER: I stored it as an integer
+// ANSWER: I stored it as a floating-point value
 //  Q2: What happens to the computed average with either data type?
-// ANSWER: Using either integer or double value it will work just fine
+// ANSWER: Using the double allows for decimals but storing as an integer does not allow for decimals
 
 public class Main {
     public static void main(String[] args) {
@@ -14,13 +14,14 @@ public class Main {
         int zip = 43215;
 
         int[] temps = {32, 25, 27, 40, 45};
-        float avg, sum = 0;
+        double avg, sum = 0;
         int length = temps.length;
         for (int temp : temps) {
             sum += temp;
         }
         avg = sum / length;
 
+        // all in one line
         System.out.println("City: Columbus " + "Zip Code: " + zip + " Average High Temperature " + avg);
     }
 }
